a6a5323d1749a2b6ecf1e0c62a9e626ce8190dc4
Clinton Sprain <clintonsprain@gmail.com>
2017-03-06 15:02:08 -0600

    reset-hidpi-settings.sh

---
+++ reset-hidpi-settings.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gsettings reset org.gnome.desktop.interface text-scaling-factor
+gsettings reset org.gnome.desktop.interface scaling-factor
+gsettings reset org.gnome.settings-daemon.plugins.xsettings overrides
+gsettings reset com.ubuntu.user-interface scale-factor
