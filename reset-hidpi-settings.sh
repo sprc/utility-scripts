13ef0fd87d90e808276b7dd99253ffd51c091453
Clinton Sprain <clintonsprain@gmail.com>
2017-03-06 15:03:18 -0600

    reset-hidpi-settings.sh

--- reset-hidpi-settings.sh
+++ reset-hidpi-settings.sh
@@ -4,3 +4,4 @@ gsettings reset org.gnome.desktop.interface text-scaling-factor
 gsettings reset org.gnome.desktop.interface scaling-factor
 gsettings reset org.gnome.settings-daemon.plugins.xsettings overrides
 gsettings reset com.ubuntu.user-interface scale-factor
+dconf write /org/gnome/shell/extensions/dash-to-panel/panel-size 32
